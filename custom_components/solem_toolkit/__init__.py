44e415fb3bae3b4b02aa9331c70cafa83c4cb75d
Dietmar Siegele <163411924+siegeledietmarfhi@users.noreply.github.com>
2026-04-19 13:26:04 +0200

    Align init module with current toolkit layout

--- custom_components/solem_toolkit/__init__.py
+++ custom_components/solem_toolkit/__init__.py
@@ -9,7 +9,7 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup(hass: HomeAssistant, config: dict):
     _LOGGER.info("Solem Toolkit loaded.")
 
-    # Registrar serviços
+    # Register services
     async_setup_services(hass)
 
     return True
