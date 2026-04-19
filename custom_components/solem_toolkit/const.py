5761f40c2ac951b0c0b508e14c5d16e34a135884
Dietmar Siegele <163411924+siegeledietmarfhi@users.noreply.github.com>
2026-04-19 13:25:55 +0200

    Expand Solem constants for BLE API

--- custom_components/solem_toolkit/const.py
+++ custom_components/solem_toolkit/const.py
@@ -1,1 +1,11 @@
+"""Constants for Solem Toolkit."""
+
 DOMAIN = "solem_toolkit"
+
+# Solem BLE characteristic used for command writes
+CHARACTERISTIC_UUID = "108b0002-eab5-bc09-d0ea-0b8f467ce8ee"
+NOTIFY_CHARACTERISTIC_UUID = "108b0003-eab5-bc09-d0ea-0b8f467ce8ee"
+
+# Default Bluetooth connection timeout (seconds)
+DEFAULT_BLUETOOTH_TIMEOUT = 15
+MIN_BLUETOOTH_TIMEOUT = 5
